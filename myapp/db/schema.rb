8a06d3b9035b8f4fca2ac41663ac5626cc711ea2
bklynate <nathanielcfa@gmail.com>
2015-08-02 10:57:54 -0400

    working through lesson one of tealeaf - added posts user and usergroup models

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731190024) do
+ActiveRecord::Schema.define(version: 20150801235658) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
